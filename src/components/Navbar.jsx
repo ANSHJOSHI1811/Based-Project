7f0b49845ba0026f34aee955e4bed1b413ae2c61
deepakshi15 <105155462+deepakshi15@users.noreply.github.com>
2024-10-13 18:28:35 +0000

    updated navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,42 +2,32 @@ import React from 'react';
 
 const Navbar = () => {
   return (
-    <nav className="bg-[#121215] text-white p-6">
+    <nav className="bg-[#121215] text-white p-3">
       <div className="container mx-auto flex justify-between items-center">
-        {/* Logo / Site Name */}
-        <div className="text-2xl font-bold">MyWebsite</div>
-
-        {/* Buttons in the Center */}
+        <div className="text-2xl font-bold">Holori</div>
         <div className="flex space-x-4">
           <button
-            className="px-6 py-3 text-lg font-medium rounded-lg 
-            bg-gradient-to-r from-[#7c4bed] to-[#3433ea] hover:opacity-80"
+            className="flex items-center px-4 py-2 text-lg font-medium rounded-lg 
+  bg-gradient-to-r from-[#7c4bed] to-[#3433ea] hover:opacity-80"
           >
-            Button 1
+            <img src="https://cdn-icons-png.flaticon.com/128/2139/2139855.png" alt="symbol" className="w-5 h-5 mr-2" />
+            Compute
           </button>
           <button
-            className="px-6 py-3 text-lg font-medium rounded-lg 
-            bg-gradient-to-r from-[#7c4bed] to-[#3433ea] hover:opacity-80"
+            className="flex items-center px-4 py-2 text-lg font-medium rounded-lg 
+  bg-gradient-to-r from-[#7c4bed] to-[#3433ea] hover:opacity-80"
           >
-            Button 2
+            <img src='https://cdn-icons-png.flaticon.com/128/9542/9542653.png' alt="symbol" className='w-5 h-5 mr-2' />
+            Storage
           </button>
         </div>
-
-        {/* Navigation Links */}
-        <div className="space-x-6">
-          <a href="#" className="hover:text-gray-400">
-            Home
-          </a>
-          <a href="#" className="hover:text-gray-400">
-            About
-          </a>
-          <a href="#" className="hover:text-gray-400">
-            Services
-          </a>
-          <a href="#" className="hover:text-gray-400">
-            Contact
-          </a>
-        </div>
+          <button
+            className="flex items-center px-4 py-2 text-lg font-medium rounded-lg 
+  bg-gradient-to-r from-[#7c4bed] to-[#3433ea] hover:opacity-80"
+          >
+            <img src='https://cdn-icons-png.flaticon.com/128/570/570160.png' alt="symbol" className='w-5 h-5 mr-2'/>
+            Visualize & optimize cloud spend
+          </button>
       </div>
     </nav>
   );
